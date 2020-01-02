122b7b0bdb782fb7c9f05cab7fa87d810fd44b3f
wangmeng <510782645@qq.com>
2020-01-02 21:06:17 +0800

    添加Eureka测试相关代码2

--- eureka-server/src/main/java/com/barrywang/eureka/EurekaServer.java
+++ eureka-server/src/main/java/com/barrywang/eureka/EurekaServer.java
@@ -1,5 +1,6 @@
 package com.barrywang.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
